c64de1f8b21e6396f048879853f9372b360d5cdc
DVORAK <cd0884213@otc.edu>
2018-09-06 10:56:48 -0500

    end of assignment

--- function.js
+++ function.js
@@ -1,4 +1,7 @@
 // Square the given number x
+for (var x = 0; x <= 10; x++) { console.log(square1(x)) }
+
+
 function square1(x) {
     x = Math.pow(x,2);
     return x;
@@ -16,3 +19,19 @@ console.log(square1(5)); // Must show 25
 console.log(square2(0)); // Must show 0
 console.log(square2(2)); // Must show 4
 console.log(square2(5)); // Must show 25
+
+function min(x,y){
+    if (x > y){
+        return y
+    } else if (x<y ){
+        return x
+    } else if (x = y) {
+        return x || y;
+    }
+
+}
+
+
+console.log(min(4.5, 5)); // Must show 4.5
+console.log(min(19, 9));  // Must show 9
+console.log(min(1, 1));   // Must show 1
