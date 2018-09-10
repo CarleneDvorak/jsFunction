9bd389217a7dfefb1dd3175a41a1ddbdcc5bca32
DVORAK <cd0884213@otc.edu>
2018-09-10 08:12:56 -0500

    fixed

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-// Square the given number x
+/* Square the given number x
 for (var x = 0; x <= 10; x++) { console.log(square1(x)) }
 
 
@@ -35,3 +35,30 @@ function min(x,y){
 console.log(min(4.5, 5)); // Must show 4.5
 console.log(min(19, 9));  // Must show 9
 console.log(min(1, 1));   // Must show 1
+*/
+
+// new code below! ^-^
+
+// Square the given number x
+function square1(x) 
+{
+    x = Math.pow(x, 2);
+    return x;
+}// TODO: complete the function code
+
+// Square the given number x
+
+const square2 = x => Math.pow(x, 2);// TODO: complete the function code
+
+console.log(square1(0)); // Must show 0
+console.log(square1(2)); // Must show 4
+console.log(square1(5)); // Must show 25
+
+console.log(square2(0)); // Must show 0
+console.log(square2(2)); // Must show 4
+console.log(square2(5)); // Must show 25
+
+for (x = 0; x <=10; x++){
+  console.log(square1(x));  
+   
+}
